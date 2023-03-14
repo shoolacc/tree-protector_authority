c0ea88f719191d0f460ff072a54c2aa3866a4ad4
shoolacc <123706564+shoolacc@users.noreply.github.com>
2023-03-14 11:42:09 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
 })
+radio.setGroup(4)
+basic.showIcon(IconNames.Yes)
+basic.showString("Scanning trees...")
